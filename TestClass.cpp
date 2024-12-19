1feaebaf8cc455dba741a0532a623758cbd83d92
Sohrab Moradi <mohamadjavad1998@outlook.com>
2024-12-19 16:55:27 +0330

    feat: worker 1 added to TestClass

--- TestClass.cpp
+++ TestClass.cpp
@@ -3,3 +3,11 @@
 TestClass::TestClass(QObject *parent) :
     QObject {parent}
 {}
+
+qint64
+TestClass::worker1()
+{
+    if(m_numbers.empty()) return 0;
+
+    return std::accumulate(m_numbers.begin(), m_numbers.end(), 1, std::multiplies<qint64>());
+}
--- TestClass.h
+++ TestClass.h
@@ -1,5 +1,4 @@
-#ifndef TESTCLASS_H
-#define TESTCLASS_H
+#pragma once
 
 #include <QObject>
 
@@ -10,7 +9,12 @@ class TestClass : public QObject
 public:
     explicit TestClass(QObject *parent = nullptr);
 
+    qint64 worker1();
+
 signals:
+
+private:
+    pthread_t        m_thread;
+    std::vector<int> m_numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 };
 
-#endif    // TESTCLASS_H
